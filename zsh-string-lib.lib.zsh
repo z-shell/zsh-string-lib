d609dcb969e3d81e69e99270060468da1f2c46ad
Salvydas Lukosius <sall@w-ss.io>
2021-12-09 14:18:39 +0000

    +zunit workflow 	new file:   .github/workflows/zunit.yml 	renamed:    @str-dump -> functions/@str-dump 	renamed:    @str-ng-match -> functions/@str-ng-match 	renamed:    @str-ng-matches -> functions/@str-ng-matches 	renamed:    @str-parse-json -> functions/@str-parse-json 	renamed:    @str-read-all -> functions/@str-read-all 	renamed:    @str-read-ini -> functions/@str-read-ini 	renamed:    @str-read-toml -> functions/@str-read-toml 	modified:   zsh-string-lib.lib.zsh

--- zsh-string-lib.lib.zsh
+++ zsh-string-lib.lib.zsh
@@ -1,11 +1,12 @@
 # According to the Zsh Plugin Standard:
 # https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
+
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
-} 
+}
 
 zmodload zsh/system 2>/dev/null
 
