b0a068c290bcbef751f4fa8ec9f373e7876f28a2
Salvydas Lukosius <sall@w-ss.io>
2022-04-07 07:49:22 +0100

    Update zsh-string-lib.lib.zsh

--- zsh-string-lib.lib.zsh
+++ zsh-string-lib.lib.zsh
@@ -1,15 +1,18 @@
-# According to the Zsh Plugin Standard:
-# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
-
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+# Standardized $0 Handling
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+# Functions directory
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#funtions-directory
 if [[ $PMSPEC != *f* ]] {
-  fpath+=( "${0:h}/functions" )
+    fpath+=( "${0:h}/functions" )
 }
 
 zmodload zsh/system 2>/dev/null
 
+# API-like functions
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
 autoload -Uz \
     @str-parse-json \
     @str-read-all \
